5fd2f8dfefaa84b8af9768dcfeeb1ff9305c6a8d
OahidZihad <oahidzihad1@gmail.com>
2021-05-04 03:03:21 +0600

    date updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import "./App.css";
+import News from "./Components/News/News";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=tesla&from=2021-05-03&sortBy=publishedAt&apiKey=5d02341651aa48b69ed59a0c8b90d961"
+    )
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>top articles: {articles.length}</h1>
+      <Button color="primary">Hello World</Button>
+      {/* <News></News> */}
+      {articles.map((article) => (
+        <News article={article}></News>
+      ))}
     </div>
   );
 }
---
+++ src/Components/News/News.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+const News = (props) => {
+  const { title, description, urlToImage, url } = props.article;
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={urlToImage}
+            title="Contemplative Reptile"
+          />
+          {/* <Avatar alt="src" src={Lizard} variant="square" /> */}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary" href={url} target="_blank">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      <br />
+    </div>
+  );
+};
+
+export default News;
