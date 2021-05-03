85603e611efe9f65b15e65c36b486a55ac742ca6
OahidZihad <oahidzihad1@gmail.com>
2021-05-04 04:54:38 +0600

    Material Ui and Axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Button } from "@material-ui/core";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 import News from "./Components/News/News";
 
 function App() {
   const [articles, setArticles] = useState([]);
+  // useEffect(() => {
+  //   fetch(
+  //     "https://newsapi.org/v2/everything?q=tesla&from=2021-05-03&sortBy=publishedAt&apiKey=5d02341651aa48b69ed59a0c8b90d961"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setArticles(data.articles));
+  // }, []);
   useEffect(() => {
-    fetch(
-      "https://newsapi.org/v2/everything?q=tesla&from=2021-05-03&sortBy=publishedAt&apiKey=5d02341651aa48b69ed59a0c8b90d961"
-    )
-      .then((res) => res.json())
-      .then((data) => setArticles(data.articles));
+    const url =
+      "https://newsapi.org/v2/everything?q=tesla&from=2021-05-03&sortBy=publishedAt&apiKey=5d02341651aa48b69ed59a0c8b90d961";
+    axios(url).then((data) => setArticles(data.data.articles));
   }, []);
   return (
     <div>
